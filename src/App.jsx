885acf8022d72693eb1d72cce2c011778407d0d0
dirkkrimpenfort <dkrim@gmx.de>
2023-07-04 14:15:59 +0200

    test-prep-spa-adv done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Card from './components/Card';
 import Footer from './components/Footer';
+import { ThemeContext } from './ThemeContext';
 import './App.css'
 
 function App() {
+  const [theme, setTheme] = useState('light');
 
+  const toggleTheme = () => {
+    setTheme(theme => theme === 'light' ? 'dark' : 'light');
+  }
 
   const country = {
     name: "South Africa",
-    img: "https://de.wikipedia.org/wiki/Datei:Flag_of_South_Africa.svg",
+    img: "public/banner-ge54d5f2f2_1920.png",
     capital: "Pretoria",
     largestCity: "Johannesburg",
     population: 58048332,
@@ -36,13 +42,13 @@ function App() {
 
 
   return (
-    <>
-      <div className='App'>
-          <Header/>
-          <Card/>
-          <Footer/>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className={`App ${theme}`}>
+        <Header/>
+        <Card country={country}/>
+        <Footer/>
       </div>
-    </>
+    </ThemeContext.Provider>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+// components/Card.jsx
+import React, { useContext } from 'react';
+import { ThemeContext } from '../ThemeContext';
 
-const Card = () => {
+const Card = ({ country }) => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
   return (
-    <div className='Card'>
-        Card
-      {/* Display Country Card with data from App here */}
+    <div className={`Card ${theme}`}>
+      <h2>{country.name}</h2>
+      <img src={country.img} alt={country.name} />
+      <p>Capital: {country.capital}</p>
+      <p>Largest City: {country.largestCity}</p>
+      <p>Population: {country.population}</p>
+      <p>Currency: {country.currency}</p>
+      <p>Official Languages: {country.officialLanguages.join(', ')}</p>
+      <p>Neighbouring Countries: {country.neighbouringCountries.join(', ')}</p>
+      <button onClick={toggleTheme}>Change Theme</button>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
